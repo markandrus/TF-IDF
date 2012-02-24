5f1f6c12e5dbc84bb20d8c8779100303489da859
Mark <markandrusroberts@gmail.com>
2012-02-23 21:07:29 -0600

    Working classifier

--- Main.hs
+++ Main.hs
@@ -1,162 +1,171 @@
 import Control.Monad
 import Data.Char
+import Data.Function (on)
 import Data.List
-import qualified Data.Text as T
+import Data.Ord (comparing)
+import Data.Maybe
+import Text.Printf
 import System.Directory
 import System.IO
 
-{- Type Synonyms & Variables -}
-
 type Author = String
+type Corpus = [(String, Int)]
+type TFIDF = [(String, Float)]
 
-directory = "federalist/"
+dir :: FilePath
+dir = "federalist/"
 
-filenamePrefixes :: [Author]
-filenamePrefixes = ["hamilton", "madison", "unknown"]
+authors :: [Author]
+authors = ["hamilton", "madison", "unknown"]
 
 -- | stopWords referenced from http://www.textfixer.com/resources/common-english-words.txt
 stopWords = ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your"]
 
-{- Preprocessing -}
-
--- | countWordsWithPlurals takes a (large) string and produces a list of Count/Words-tuples,
---   counting both plurals and regular words
---
--- NOTE: see README for the number of words reduced in this use case by noStopWords and f (the
---       stem recognizer)
-countWordsWithPlurals :: String -> [(Int, [T.Text])]
-countWordsWithPlurals = map (\ws -> (length ws, ws))
-                      . groupBy f
-                      . map T.pack
-                      . sort
-                      . noStopWords
-                      . alphaOnly
-                      . words
-                      . map toLower
-  where
-    -- | noStopWords removes the likely-to-be-insignificant stop words within the text
-    noStopWords = filter (`notElem` stopWords)
-    alphaOnly = map (filter isAlpha)
-    -- | f is an (English) stem recognizer
-    f a b = case T.commonPrefixes a b of
-      Nothing -> False -- No common prefix, therefore this is not a (simple) plural
-      Just (pre, suf1, suf2) ->
-        -- Rules for (simple) English plurals, according to 
-        -- http://en.wikipedia.org/wiki/English_plural
-        if (suf1==suf2)
-          || (suf1==T.empty && suf2==T.pack "s")
-          || (suf1==T.empty && suf2==T.pack "es")
-          || (suf1==T.pack "y" && suf2==T.pack "ies")
-        then True
-        else False
-
--- | redoCountWordsWithPlurals is an expensive hack, but I don't have time to build something
---   betters; after individually counting documents, we build a corpus of words and wordcounts
---   with this (again, very expensive) combining function
-redoCountWordsWithPlurals :: [(Int, [T.Text])] -> [(Int, [T.Text])]
-redoCountWordsWithPlurals = countWordsWithPlurals
-                          . concat
-                          . tail
-                          . map (\x -> ' ' : T.unpack x)
-                          . concat
-                          . snd
-                          . unzip
-
--- | noDots  makes sure we don't try to read `.' or `..'
+-- |noDots removes the `.' and `..' directory listings
 noDots :: [FilePath] -> [FilePath]
-noDots = filter (\x->x/="."&&x/="..")
+noDots = filter (\x -> x/="." && x/="..")
 
--- | groupPapers groups papers by the same author
+-- |commonPrefix returns the common prefix of two lists
+commonPrefix :: Eq a => [a] -> [a] -> [a]
+commonPrefix x y = fst . unzip . fst . span (uncurry (==)) $ zip x y
+
+-- |groupPapers groups papers by the same author
 groupPapers :: [FilePath] -> [[FilePath]]
-groupPapers = groupBy (\x y -> takeWhile isAlpha y `isPrefixOf` x)
+groupPapers = groupBy (\x y -> any (`isPrefixOf` commonPrefix x y) authors)
 
--- | readPapers takes a sorted directory listing containing files of `$filenamePrefix[0-9]+\.txt',
---
---   This is the type I would like:
---
---     readPapers :: [FilePath] -> IO [(Author, [[(Int, [T.Text])]])]
---
---   But the liftM and mapM and fmap stuff gets confusing... so I guess we could zip in main with
---   filenamePrefixes?
---
-readPapers :: [FilePath] -> IO [[[(Int, [T.Text])]]]
-readPapers = mapM (mapM (fmap countWordsWithPlurals . readFile))
-           . groupPapers -- [[]]
-           . prependPath
-           . noDots
+-- |isPlural determines whether the latter of two (English) words is a (simple) pluralization of
+--  the first
+isPlural :: String -> String -> Bool
+isPlural a b = case commonPrefix a b of
+  "" -> False
+  pre -> (suf1==""  && suf2=="s")
+      || (suf1==""  && suf2=="es")
+      || (suf1=="y" && suf2=="ies")
+      -- NOTE: I didn't catch this until later, but consider the fact that
+      --       `sort ["apply","applies"]` produces `["applies","apply"]`
+      || (suf2==""  && suf1=="s")
+      || (suf2==""  && suf1=="es")
+      || (suf2=="y" && suf1=="ies")
+    where
+      suf1 = drop (length pre) a
+      suf2 = drop (length pre) b
+
+-- |isSameOrPlural determines whether the two words are the same or if the latter of the two is a
+--  (simple, English) pluralization of the first
+isSameOrPlural :: String -> String -> Bool
+isSameOrPlural a b = (a==b) || isPlural a b
+
+-- |uniques takes a list of words and returns a list of word/count-tuples, taking into account
+--  (simple) English pluralizations
+makeCorpus :: [String] -> Corpus
+makeCorpus = map (\(x:_) -> (x, length x))
+           . groupBy isSameOrPlural
+           . filter (`notElem` stopWords)
+           . sort
+           . map (map toLower . filter isAlpha)
+
+-- |unionUniques takes two lists of word/count-tuples and combines them, taking into account
+--  (simple) English pluralizations
+unionCorpus :: [Corpus] -> Corpus
+unionCorpus (x:xs) = foldr (\a b -> reduce $ a++b) x xs
   where
-    -- | prependPath adds the directory back in for readFile
-    prependPath = map (directory++)
+    -- |reduce combines the counts of identical and plural words
+    reduce :: Corpus -> Corpus
+    reduce = map (foldr (\(w',c') (w,c) -> (w',c+c')) ("",0))
+           . groupBy (isSameOrPlural `on` fst)
+           . sortBy (comparing fst)
 
-{- Classifier -}
-
--- | thetas computes the likelihood of each words of a given document occuring, and takes into
---   account a given pseudocount gamma and constant multiplier k (to be determined through cross-
---   validation); equivalent to:
---
---     \hat\theta=(\hat\theta_1...\hat\theta_k) where \hat\theta_i=\frac{x_i}{x_1+...+x_k}
---
-thetas :: (Integral a1, Fractional b) => a1 -> a1 -> [(a1, [a])] -> [(a, b)]
-thetas k gamma ws = zip firstWords $ map (\(c,_) -> fromIntegral (c + gamma) / totalCount) ws
+-- |makeTFIDF computes the "term frequency--inverse document frequency" of a Corpus given the
+--  per-document Corpuses which compose it
+makeTFIDF :: Corpus -> [Corpus] -> TFIDF
+makeTFIDF corpus corpusPerDoc = normalize
+                              $ map (\(word,count) ->
+                                      ( word
+                                        -- tf * idf
+                                      , fromIntegral count * docCount
+                                          / (1.0 + countOccurs word corpusPerDoc)
+                                      )
+                                    ) corpus
   where
-    (counts, words) = unzip ws
-    totalCount = fromIntegral $ k*gamma + sum counts
-    firstWords = map head words
-
--- | likelihood computes the likelihood that a given document, ws, was written by the same author
---   of ds; requires a k and gamma value to pass to thetas; equivalent to:
---
---     \mathcal{l}(\theta)=\prod_{t=1}^m p(x_t|\theta)
---
---likelihood :: (Integral a1, Fractional b) => a1 -> a1 -> [(a1, [a])] -> [(a1, [a])] -> [b]
-{-
-likelihood k gamma ds ws = map (\w -> foldr ((*) . (w `given` (thetas k gamma))) 1 ds') ws
-  where
-    ds' = snd $ unzip ds
-    given (c,words) b =
-      case (T.unpack $ head words) `lookup` uncurry (\theta words ->
-          zip (map (T.unpack . head) words) theta) b of
-        Nothing -> 0.0
-        Just t -> c * t -- NOTE: revise
--}
-
-{- Cross-validation -}
-
--- | crossValidate takes a learning algorithm, a classifier, and a list of examples
-crossValidate l xs = map f [n-a | a <- [1..n-1]] where
-  n = length xs
-  f i = l training tests where (training, tests) = splitAt i xs
-
-{- Main -}
+    -- |normalize normalizes the frequencies
+    normalize tfidf = let m = sum . snd $ unzip tfidf in map (\(w,f) -> (w,f/m)) tfidf
+    -- |countOccurs, given a word, and a list of corpuses, sums the number of occurences of said
+    --  word, returning it as a Float
+    countOccurs :: String -> [Corpus] -> Float
+    countOccurs word = fromIntegral
+                     . foldr ((+) . fromJust) 0
+                     . filter isJust
+                     . map (lookup word)
+    -- |docCount is the number of documents as a Float
+    docCount :: Float
+    docCount = fromIntegral $ length corpusPerDoc
 
 main :: IO ()
 main = do
-  files <- getDirectoryContents directory
-  -- NOTE: the following comprehension is a hack, since I can't figure out the correct way to
-  --       fulfill the type I want for readPapers
-  docs':_ <- readPapers files
-  docs <- zipWithM (curry return) (noDots files) docs'
-  corpusWords' <- foldM (\w (filename, words) -> do
-      let wordCount = length words
-      hPutStrLn stderr $ show wordCount++" unique words in `"++filename++"'"
-      return (w++words)
-    ) [] (take 30 docs) -- NOTE: this isn't the most elegant way to do it, but I am short on time
-                        --       and I know the first 30 are *not* unknown
-  let corpusWords = redoCountWordsWithPlurals corpusWords'
-  let corpusWordCount = length corpusWords
-  hPutStrLn stderr $ "\n"++show corpusWordCount++" unique words across all documents\n"
-  -- NOTE: same comment about take 30 as above
-  let grouped = uncurry (\files words -> zip (groupPapers files) words) . unzip $ take 30 docs
-  -- NOTE: same comment about take 30 as above
-  let grouped' = uncurry (\files words -> zip files words) . unzip $ take 30 docs
-  -- NOTE: same comment about take 15 as above
-  let hamilton = snd . unzip $ take 15 grouped'
-  let madison = snd . unzip . take 15 $ drop 15 grouped'
-  mapM_ (hPutStrLn stderr . show . crossValidate (likelihood 1 0)) hamilton
-  docThetas <- forM grouped (\(file, words) -> do
-    let docThetas = thetas 1 0 words
-    hPutStrLn stderr $ "Author: "++show file
-    hPutStrLn stderr $ show docThetas
-    return (file, docThetas)
+  files <- (zip authors . groupPapers . noDots) `liftM` getDirectoryContents dir
+  corpusPerAuthor <- forM files (\(author, docs) -> do
+
+      hPrintf stderr "Author: %s\n  Files: %d\n" author (length docs)
+
+      (wordsPerDoc, corpusPerDoc) <- unzip `liftM`
+        forM docs (\filePath -> do
+            wordsFromFile <- words `liftM` readFile (dir++filePath)
+            let corpus = makeCorpus wordsFromFile
+            hPrintf stderr "    File: %s\n" filePath
+            hPrintf stderr "      Words:   %d\n" (length wordsFromFile)
+            hPrintf stderr "      Uniques: %d\n" (length corpus)
+            return (wordsFromFile, corpus)
+          )
+
+      let totalWords = foldr ((+) . length) 0 wordsPerDoc
+      hPrintf stderr "  Total Words:   %d\n" totalWords
+
+      let corpus = unionCorpus corpusPerDoc
+      hPrintf stderr "  Total Uniques: %d\n\n" (length corpus)
+
+      return (author, (corpus, corpusPerDoc))
     )
-  hPutStrLn stderr $ "Going"
+
+  let authorTFIDFs = map (\(author, (corpus, corpusPerDoc)) ->
+                           ( author
+                           , makeTFIDF corpus corpusPerDoc
+                           )
+                         ) corpusPerAuthor
+
+  forM_ authorTFIDFs (\(author, tfidf) -> do
+      hPrintf stderr "TFIDF for Author: %s\n" author
+      forM_ (sortBy (comparing snd) tfidf) (\(word, freq) -> do
+          hPrintf stderr "  Frequency: %f  Word: %s\n" freq word
+        )
+      hPutStrLn stderr ""
+    )
+
+  -- NOTE: we know the first two
+  let known = take 2 authorTFIDFs :: [(Author, TFIDF)]
+
+  putStrLn "Analysis of unknown documents"
+  forM_ (zip (fromJust $ lookup "unknown" files)
+             (snd . fromJust $ lookup "unknown" corpusPerAuthor)
+        ) (\(file, corpus) -> do
+      printf "  File: %s\n" file
+      let m = fromIntegral (sum . snd $ unzip corpus) :: Float
+      let corpus' = map (\(word, count) -> (word, fromIntegral count / m)) corpus
+      let fromJust' j | isNothing j = 0
+                      | otherwise = fromJust j
+      let differences = sortBy (comparing snd)
+                      . zip authors
+                      $ map (\(author, authorTFIDF) ->
+                              foldr (\(word,freq) a ->
+                                      a + (fromJust' $ lookup word authorTFIDF) - freq
+                                    ) 0.0
+                              -- NOTE: when using intersectBy, the element from the first list is
+                              --       the one retained
+                            $ intersectBy (\(word,_) (authorWord,_) ->
+                                            word `isSameOrPlural` authorWord
+                                          ) corpus' authorTFIDF
+                            ) known
+      forM_ (differences) (\(author,diff) -> do
+          printf "      Author: %s\n        Difference: %f\n" author diff
+        )
+      printf "   Chose: %s\n\n" . fst $ head differences
+    )
